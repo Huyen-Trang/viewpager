69026291ce17863e026daac838f0b3de88ed3e40
Huyen-Trang <huyentranghathi@gmail.com>
2018-03-11 03:40:16 +0700

    first commit

---
+++ app/src/main/java/com/tranght/administrator/viewpager/TabFragment1.java
@@ -0,0 +1,28 @@
+package com.tranght.administrator.viewpager;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link android.support.v4.app.Fragment} subclass.
+ */
+public class TabFragment1 extends android.support.v4.app.Fragment {
+
+
+    public TabFragment1() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.tab_fragment_1, container, false);
+    }
+
+}
